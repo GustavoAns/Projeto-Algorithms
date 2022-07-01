19bd12353082c4efc57d6b2e298885098cd74b1c
GustavoAns <gugasato9@live.com>
2022-07-01 17:02:48 -0300

    Add req 4

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,12 @@
+from collections import Counter
+
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    try:
+        most_common = Counter(nums).most_common(1)[0]
+        if type(most_common[0]) is str or most_common[0] < 0:
+            return False
+        if most_common[1] > 1:
+            return most_common[0]
+        return False
+    except IndexError or ValueError:
+        return False
